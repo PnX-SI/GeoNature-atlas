2130a261f38ca6c1b32d31b6e66bede4a3a332dd
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2016-06-07 14:37:42 +0200

    mise en place d'un répertoire de config

--- __init__.py
+++ __init__.py
@@ -3,13 +3,14 @@ import sys
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from config import config, database_connection
-
 
 appDir = os.path.abspath(os.path.dirname(__file__))
 baseDir = os.path.abspath(os.path.join(appDir, os.pardir))
 templateDir = appDir+'/templates'
+configDir = appDir+'/config'
 
+sys.path.insert(0, configDir)
+from config import config, database_connection
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -22,7 +23,6 @@ def create_app(config_name):
     engine = create_engine(database_connection, client_encoding='utf8', echo = False)
     bootstrap.init_app(app)
     
-
     from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
