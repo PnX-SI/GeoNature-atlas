839623fed55025aece642ee34085b4fcd5890d25
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2016-06-13 18:09:24 +0200

    correction du fichier grant.sql

--- data/grant.sql
+++ data/grant.sql
@@ -5,23 +5,21 @@ SET search_path = public, pg_catalog;
 --
 
 REVOKE ALL ON TABLE geometry_columns FROM PUBLIC;
-REVOKE ALL ON TABLE geometry_columns FROM geonatuser;
-GRANT ALL ON TABLE geometry_columns TO geonatuser;
+REVOKE ALL ON TABLE geometry_columns FROM geonatatlas;
+GRANT ALL ON TABLE geometry_columns TO geonatatlas;
 
 --
 -- Name: geography_columns; Type: ACL; Schema: public; Owner: -
 --
 
 REVOKE ALL ON TABLE geography_columns FROM PUBLIC;
-REVOKE ALL ON TABLE geography_columns FROM geonatuser;
-GRANT ALL ON TABLE geography_columns TO geonatuser;
+REVOKE ALL ON TABLE geography_columns FROM geonatatlas;
+GRANT ALL ON TABLE geography_columns TO geonatatlas;
 
 --
 -- Name: spatial_ref_sys; Type: ACL; Schema: public; Owner: -
 --
 
 REVOKE ALL ON TABLE spatial_ref_sys FROM PUBLIC;
-REVOKE ALL ON TABLE spatial_ref_sys FROM geonatuser;
-#modif Théo
-#GRANT ALL ON TABLE spatial_ref_sys TO geonatuser;
+REVOKE ALL ON TABLE spatial_ref_sys FROM geonatatlas;
 GRANT ALL ON TABLE spatial_ref_sys TO geonatatlas;
