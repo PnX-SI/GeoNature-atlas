88f0b5351903d9761e5c77f06ffc8400dd524262
theo <lechemia.theo@gmail.com>
2016-06-22 11:22:06 +0200

    ajout orthoIgn

--- static/conf/custom.sample.js
+++ static/conf/custom.sample.js
@@ -9,6 +9,8 @@ var osmAttribution = '&copy; <a href="http://www.openstreetmap.org/copyright">Op
 
 var ignUrl='http://gpp3-wxs.ign.fr/'+ignApiKey+'/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD&EXCEPTIONS=text/xml&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}'
 var ignAttribution = '&copy; <a href="http://www.ign.fr/">IGN</a>'
+var orthoIGN = 'https://gpp3-wxs.ign.fr/'+ignApiKey+'/geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=text/xml&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}';
+
 
 
 //**********
