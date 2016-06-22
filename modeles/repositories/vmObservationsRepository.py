be58b3cc31cfd7548af60a9bc02b8cffd58d2143
theo <lechemia.theo@gmail.com>
2016-06-22 23:53:41 +0200

    bug fix

--- modeles/repositories/vmObservationsRepository.py
+++ modeles/repositories/vmObservationsRepository.py
@@ -15,13 +15,37 @@ session = manage.loadSession()
 
 currentYear = datetime.now().year
 
-def toGeoJson(queryResult, nbYear):
+def toGeoJsonTaxon(queryResult, nbYear):
+    geojson = {'type': 'FeatureCollection',
+           'features' : list()
+          }
+    for r in queryResult:
+        geometry = ast.literal_eval(r.geojson_point)
+        properties = {'id_synthese' : r.id_synthese,
+                      'cd_ref': r.cd_ref,
+                      'dateobs': str(r.dateobs),
+                      'observateurs' : r.observateurs,
+                      'altitude_retenue' : r.altitude_retenue,
+                      'effectif_total' : r.effectif_total,
+                      'year': r.dateobs.year
+                      }
+        feature = {
+            'type' : 'Feature',
+            'properties' : properties,
+            'geometry' : geometry
+        }
+
+        geojson['features'].append(feature)
+
+    return geojson
+
+def toGeoJsonHome(queryResult):
     geojson = {'type': 'FeatureCollection',
            'features' : list()
           }
     for r in queryResult:
         geometry = ast.literal_eval(r.VmObservations.geojson_point)
-        #test if nom_vern not is null because 'none' isn't supported for concat
+        # test if nom_vern not is null because 'none' isn't supported for concat
         if r.VmTaxref.nom_vern:
             taxon = r.VmTaxref.nom_vern + ' | ' + r.VmTaxref.lb_nom
         else:
@@ -48,16 +72,8 @@ def toGeoJson(queryResult, nbYear):
 
 def searchObservation(cd_ref):
     observations = session.query(VmObservations).filter(VmObservations.cd_ref == cd_ref).all()
-    return  toGeoJson(observations, 15)
+    return  toGeoJsonTaxon(observations, 15)
     
 def lastObservations(mylimit):
     observations = session.query(VmObservations,VmTaxref).join(VmTaxref, VmObservations.cd_ref==VmTaxref.cd_nom).order_by(desc(VmObservations.dateobs)).limit(100).all()
-    # observations = session.query(VmObservations).order_by(desc(VmObservations.dateobs)).limit(mylimit).all()
-    return  toGeoJson(observations, 15)
-    
-   
-
-         
-
-
-
+    return  toGeoJsonHome(observations)
