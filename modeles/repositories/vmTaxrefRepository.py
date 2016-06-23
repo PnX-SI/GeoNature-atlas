d8dc4081d5d2888ab1b41006b4a7ccb7ec659f62
theo <lechemia.theo@gmail.com>
2016-06-23 17:37:30 +0200

    modif taxonomie

--- modeles/repositories/vmTaxrefRepository.py
+++ modeles/repositories/vmTaxrefRepository.py
@@ -14,15 +14,3 @@ session = manage.loadSession()
 
 def getSynonymy(cd_ref):
     return session.query(VmTaxref.lb_nom).filter(VmTaxref.cd_ref==cd_ref).all()
-
-    # i = 0
-    # for x in req:
-    #     i = i+1
-    # return {"synonyme" : req,
-    #          "nbSynonyme": i,
-    #          "ordre"}
-
-def getTaxonomy(cd_ref):
-	req= session.query(VmTaxref.ordre, VmTaxref.famille).filter(VmTaxref.cd_ref==cd_ref).limit(1).all()
-	return req[0]
-
