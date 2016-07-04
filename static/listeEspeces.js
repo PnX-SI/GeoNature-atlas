64a80ae3db5a01e9fe3a1760759018a857545d0b
theo <lechemia.theo@gmail.com>
2016-07-04 16:07:30 +0200

    fiche commune: recherche et tri -cf #32

---
+++ static/listeEspeces.js
@@ -0,0 +1,44 @@
+
+/*// sort the table
+$(document).ready(function() 
+    { 
+        $("#myTable")
+        .tablesorter();
+
+    } 
+); 
+*/
+
+
+$(document).ready(function(){
+    $('#myTable').DataTable({
+    	"order":[2, 'desc'],
+    	"lengthChange": false,
+    	"pageLength": 50,
+        "oLanguage": {
+           "sSearch": "",
+            "sInfo": "",
+            "sInfoEmpty": "",
+            "sInfoFiltered": "",
+            "sZeroRecords": "Aucune espèce trouvée",
+
+        "oPaginate": {
+           "sPrevious": "Précedent",
+           "sNext" : "Suivant"
+         }
+      }
+
+    });
+    $('.dataTables_filter input').attr("placeholder", "Rechercher dans la liste ").attr("class", "form-control").css("font-weight", "normal");
+    $('.dataTables_empty').text("rien")
+
+});
+
+
+// change de glyphicon
+
+$('th').click( function(){
+    $(this).find('span').toggleClass('glyphicon glyphicon-menu-down').toggleClass('glyphicon glyphicon-menu-up');
+});
+
+    
