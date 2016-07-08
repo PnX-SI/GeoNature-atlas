d86df2315442e1cc45ae032182485de093b8e882
theo <lechemia.theo@gmail.com>
2016-07-08 12:13:38 +0200

    remonte la hierarchie taxonomique superieure pour chaque espece -cf #4

--- modeles/repositories/vmTaxrefRepository.py
+++ modeles/repositories/vmTaxrefRepository.py
@@ -14,3 +14,25 @@ session = manage.loadSession()
 
 def getSynonymy(cd_ref):
     return session.query(VmTaxref.lb_nom).filter(VmTaxref.cd_ref==cd_ref).all()
+
+
+def getCd_ref(cd_nom):
+    req = session.query(VmTaxref.cd_ref).filter(VmTaxref.cd_nom == cd_nom).all()
+    return req[0].cd_ref
+
+def getTaxon(cd_nom):
+    req = session.query(VmTaxref.lb_nom, VmTaxref.id_rang, VmTaxref.cd_ref, VmTaxref.cd_taxsup).filter(VmTaxref.cd_nom == cd_nom)
+    return req[0]
+
+
+def getAllTaxonomy(cd_ref):
+    taxon = getTaxon(cd_ref)
+    tabTaxon = list()
+    i = 0
+    while 'CL' not in taxon.id_rang.encode('UTF-8'): 
+        temp = {'rang' : taxon.id_rang, 'lb_nom' : taxon.lb_nom, 'cd_ref': taxon.cd_ref}
+        tabTaxon.insert(0, temp)
+        cd_ref_sup = getCd_ref(taxon.cd_taxsup) #renvoie le premier cd_ref du cd_nom
+        taxon = getTaxon(cd_ref_sup) #on avance
+        i = i+1
+    return tabTaxon
