2e08005f06f7f6fc3dfbb81f6062104143a8f3b4
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2016-07-12 15:42:31 +0200

    suppression d'une fonction générant des erreurs si trous dans la taxonomie atlas

--- data/atlas.sql
+++ data/atlas.sql
@@ -557,43 +557,6 @@ create index on atlas.vm_taxref (lb_nom);
 create index on atlas.vm_taxref (nom_complet);
 create index on atlas.vm_taxref (nom_valide);
 
---DROP FUNCTION atlas.find_atlas_taxons_childs(id integer);
-CREATE OR REPLACE FUNCTION atlas.find_atlas_taxons_childs(id integer)
-  RETURNS SETOF integer AS
-$BODY$
- --Param : cd_nom ou cd_ref d'un taxon quelque soit son rang
- --Retourne le cd_nom de tous les taxons enfants sous forme d'un jeu de données utilisable comme une table
- --Usage SELECT atlas.find_atlas_taxons_childs(197047); 
- --ou SELECT * FROM atlas.vm_taxons WHERE cd_ref IN(SELECT * FROM atlas.find_atlas_taxons_childs(197047))
-  DECLARE 
-    inf RECORD;
-    c integer;
-  BEGIN
-    SELECT INTO c count(*) FROM atlas.vm_taxref WHERE cd_taxsup = id;
-    IF c > 0 THEN
-        FOR inf IN 
-      WITH RECURSIVE descendants AS (
-        SELECT DISTINCT cd_nom FROM atlas.vm_taxref tx
-        JOIN atlas.vm_observations o ON o.cd_ref = tx.cd_nom 
-        WHERE cd_taxsup = id
-        UNION ALL
-        SELECT DISTINCT e.cd_nom
-        FROM descendants d
-        JOIN atlas.vm_taxref e ON e.cd_taxsup = d.cd_nom
-        JOIN atlas.vm_observations ob ON ob.cd_ref = e.cd_nom
-      ) 
-      SELECT cd_nom FROM descendants 
-  LOOP
-      RETURN NEXT inf.cd_nom;
-  END LOOP;
-    END IF;
-  END;
-$BODY$
-  LANGUAGE plpgsql IMMUTABLE
-  COST 100;
-ALTER FUNCTION atlas.find_atlas_taxons_childs(integer)
-  OWNER TO geonatatlas;
-  
 CREATE OR REPLACE FUNCTION atlas.find_all_taxons_childs(id integer)
   RETURNS SETOF integer AS
 $BODY$
