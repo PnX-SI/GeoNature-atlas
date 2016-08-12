4105ae98099536fb65b7d3bc215d7f2f9664a678
theo <lechemia.theo@gmail.com>
2016-08-12 17:17:30 +0200

    ligthbox bricolage

---
+++ static/ficheEspece.js
@@ -0,0 +1,40 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+
+carousselImg = $(".imgToCarousel")
+lisUrl = [];
+
+for (i=0; i<carousselImg.length; i++){
+	lisUrl.push(carousselImg[i].src)
+}
+console.log(carousselImg);
+console.log(lisUrl);
+
+var i = 1;
+modal.onclick = function(){
+	if(i==lisUrl.length){
+		i=1
+	}
+	modalImg.src = lisUrl[i]
+	i = i+1;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
