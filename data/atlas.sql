a61e7c3048d958eb874e3d813022bdd98d335224
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2017-04-07 16:09:43 +0200

    INSTALL - Ne plus limiter à l'organisme 2 par defaut

--- data/atlas.sql
+++ data/atlas.sql
@@ -30,7 +30,6 @@ CREATE MATERIALIZED VIEW atlas.vm_observations AS
     LEFT JOIN atlas.vm_taxref tx ON tx.cd_nom = s.cd_nom
     JOIN atlas.t_layer_territoire m ON ST_Intersects(m.the_geom, s.the_geom_point)
     WHERE s.supprime = FALSE
-    AND s.id_organisme = 2
     AND s.diffusable = TRUE;
 
 create unique index on atlas.vm_observations (id_observation);
