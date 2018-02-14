d77d6b7fe347b4fd07d382003c5d04b09bee670c
amandine-sahl <amandine.sahl@gmail.com>
2018-02-14 09:40:34 +0100

    Style - pep8

--- main/atlasRoutes.py
+++ main/atlasRoutes.py
@@ -1,14 +1,11 @@
-#! /usr/bin/python
-# -*- coding:utf-8 -*-
-from flask import Flask, request, render_template, jsonify, redirect, abort
+from flask import render_template, redirect, abort
 from configuration import config
 from modeles.repositories import (
     vmTaxonsRepository, vmObservationsRepository, vmAltitudesRepository,
-    vmSearchTaxonRepository, vmMoisRepository, vmTaxrefRepository,
+    vmMoisRepository, vmTaxrefRepository,
     vmCommunesRepository, vmObservationsMaillesRepository, vmMedias,
     vmCorTaxonAttribut, vmTaxonsMostView
 )
-import json
 from . import utils
 
 from flask import Blueprint
@@ -25,27 +22,42 @@ base_configuration = {
 }
 
 
-@main.route('/espece/'+config.REMOTE_MEDIAS_PATH+'<image>', methods=['GET', 'POST'])
+@main.route(
+    '/espece/'+config.REMOTE_MEDIAS_PATH+'<image>',
+    methods=['GET', 'POST']
+)
 def especeMedias(image):
     return redirect(config.REMOTE_MEDIAS_URL+config.REMOTE_MEDIAS_PATH+image)
 
 
-@main.route('/commune/'+config.REMOTE_MEDIAS_PATH+'<image>', methods=['GET', 'POST'])
+@main.route(
+    '/commune/'+config.REMOTE_MEDIAS_PATH+'<image>',
+    methods=['GET', 'POST']
+)
 def communeMedias(image):
     return redirect(config.REMOTE_MEDIAS_URL+config.REMOTE_MEDIAS_PATH+image)
 
 
-@main.route('/liste/'+config.REMOTE_MEDIAS_PATH+'<image>', methods=['GET', 'POST'])
+@main.route(
+    '/liste/'+config.REMOTE_MEDIAS_PATH+'<image>',
+    methods=['GET', 'POST']
+)
 def listeMedias(image):
     return redirect(config.REMOTE_MEDIAS_URL+config.REMOTE_MEDIAS_PATH+image)
 
 
-@main.route('/groupe/'+config.REMOTE_MEDIAS_PATH+'<image>', methods=['GET', 'POST'])
+@main.route(
+    '/groupe/'+config.REMOTE_MEDIAS_PATH+'<image>',
+    methods=['GET', 'POST']
+)
 def groupeMedias(image):
     return redirect(config.REMOTE_MEDIAS_URL+config.REMOTE_MEDIAS_PATH+image)
 
 
-@main.route('/'+config.REMOTE_MEDIAS_PATH+'<image>', methods=['GET', 'POST'])
+@main.route(
+    '/'+config.REMOTE_MEDIAS_PATH+'<image>',
+    methods=['GET', 'POST']
+)
 def indexMedias(image):
     return redirect(config.REMOTE_MEDIAS_URL+config.REMOTE_MEDIAS_PATH+image)
 
@@ -56,15 +68,23 @@ def index():
     connection = utils.engine.connect()
 
     if config.AFFICHAGE_MAILLE:
-        observations = vmObservationsMaillesRepository.lastObservationsMailles(connection, config.NB_DAY_LAST_OBS, config.ATTR_MAIN_PHOTO)
+        observations = vmObservationsMaillesRepository.lastObservationsMailles(
+            connection, config.NB_DAY_LAST_OBS, config.ATTR_MAIN_PHOTO
+        )
     else:
-        observations = vmObservationsRepository.lastObservations(connection, config.NB_DAY_LAST_OBS, config.ATTR_MAIN_PHOTO)
+        observations = vmObservationsRepository.lastObservations(
+            connection, config.NB_DAY_LAST_OBS, config.ATTR_MAIN_PHOTO
+        )
 
     communesSearch = vmCommunesRepository.getAllCommunes(session)
     mostViewTaxon = vmTaxonsMostView.mostViewTaxon(connection)
     stat = vmObservationsRepository.statIndex(connection)
-    customStat = vmObservationsRepository.genericStat(connection, config.RANG_STAT)
-    customStatMedias = vmObservationsRepository.genericStatMedias(connection, config.RANG_STAT)
+    customStat = vmObservationsRepository.genericStat(
+        connection, config.RANG_STAT
+    )
+    customStatMedias = vmObservationsRepository.genericStatMedias(
+        connection, config.RANG_STAT
+    )
 
     configuration = base_configuration.copy()
     configuration.update({
@@ -106,14 +126,28 @@ def ficheEspece(cd_ref):
     altitudes = vmAltitudesRepository.getAltitudesChilds(connection, cd_ref)
     months = vmMoisRepository.getMonthlyObservationsChilds(connection, cd_ref)
     synonyme = vmTaxrefRepository.getSynonymy(connection, cd_ref)
-    communes = vmCommunesRepository.getCommunesObservationsChilds(connection, cd_ref)
+    communes = vmCommunesRepository.getCommunesObservationsChilds(
+        connection, cd_ref
+    )
     communesSearch = vmCommunesRepository.getAllCommunes(session)
     taxonomyHierarchy = vmTaxrefRepository.getAllTaxonomy(session, cd_ref)
-    firstPhoto = vmMedias.getFirstPhoto(connection, cd_ref, config.ATTR_MAIN_PHOTO)
-    photoCarousel = vmMedias.getPhotoCarousel(connection, cd_ref, config.ATTR_OTHER_PHOTO)
-    videoAudio = vmMedias.getVideo_and_audio(connection, cd_ref, config.ATTR_AUDIO, config.ATTR_VIDEO_HEBERGEE, config.ATTR_YOUTUBE, config.ATTR_DAILYMOTION, config.ATTR_VIMEO)
-    articles = vmMedias.getLinks_and_articles(connection, cd_ref, config.ATTR_LIEN, config.ATTR_PDF)
-    taxonDescription = vmCorTaxonAttribut.getAttributesTaxon(connection, cd_ref, config.ATTR_DESC, config.ATTR_COMMENTAIRE, config.ATTR_MILIEU, config.ATTR_CHOROLOGIE)
+    firstPhoto = vmMedias.getFirstPhoto(
+        connection, cd_ref, config.ATTR_MAIN_PHOTO
+    )
+    photoCarousel = vmMedias.getPhotoCarousel(
+        connection, cd_ref, config.ATTR_OTHER_PHOTO
+    )
+    videoAudio = vmMedias.getVideo_and_audio(
+        connection, cd_ref, config.ATTR_AUDIO, config.ATTR_VIDEO_HEBERGEE,
+        config.ATTR_YOUTUBE, config.ATTR_DAILYMOTION, config.ATTR_VIMEO
+    )
+    articles = vmMedias.getLinks_and_articles(
+        connection, cd_ref, config.ATTR_LIEN, config.ATTR_PDF
+    )
+    taxonDescription = vmCorTaxonAttribut.getAttributesTaxon(
+        connection, cd_ref, config.ATTR_DESC, config.ATTR_COMMENTAIRE,
+        config.ATTR_MILIEU, config.ATTR_CHOROLOGIE
+    )
     observers = vmObservationsRepository.getObservers(connection, cd_ref)
 
     configuration = base_configuration.copy()
@@ -163,11 +197,17 @@ def ficheCommune(insee):
     commune = vmCommunesRepository.getCommuneFromInsee(connection, insee)
     communesSearch = vmCommunesRepository.getAllCommunes(session)
     if config.AFFICHAGE_MAILLE:
-        observations = vmObservationsMaillesRepository.lastObservationsCommuneMaille(connection, config.NB_LAST_OBS, insee)
+        observations = vmObservationsMaillesRepository.lastObservationsCommuneMaille(
+            connection, config.NB_LAST_OBS, insee
+        )
     else:
-        observations = vmObservationsRepository.lastObservationsCommune(connection, config.NB_LAST_OBS, insee)
+        observations = vmObservationsRepository.lastObservationsCommune(
+            connection, config.NB_LAST_OBS, insee
+        )
 
-    observers = vmObservationsRepository.getObserversCommunes(connection, insee)
+    observers = vmObservationsRepository.getObserversCommunes(
+        connection, insee
+    )
 
     configuration = base_configuration.copy()
     configuration.update({
