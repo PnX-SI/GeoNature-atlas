a76c7837c8ae3002315ccb858ef2b8812ccf4803
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-09-30 13:53:44 +0200

    Gallerie photo: correction affichage nb obs

--- atlas/modeles/repositories/vmMedias.py
+++ atlas/modeles/repositories/vmMedias.py
@@ -81,18 +81,24 @@ def switchMedia(row):
         return None
 
     return {
-        current_app.config['ATTR_AUDIO']: goodPath,
-        current_app.config['ATTR_VIDEO_HEBERGEE']: goodPath,
-        current_app.config['ATTR_YOUTUBE']: "<iframe width='100%' height='315' src='https://www.youtube.com/embed/"  # noqa
+        current_app.config["ATTR_AUDIO"]: goodPath,
+        current_app.config["ATTR_VIDEO_HEBERGEE"]: goodPath,
+        current_app.config[
+            "ATTR_YOUTUBE"
+        ]: "<iframe width='100%' height='315' src='https://www.youtube.com/embed/"  # noqa
         + row.url
         + "' frameborder='0' allowfullscreen></iframe>",
-        current_app.config['ATTR_DAILYMOTION']: "<iframe frameborder='0' width='100%' height='315' src='//www.dailymotion.com/embed/video/"  # noqa
+        current_app.config[
+            "ATTR_DAILYMOTION"
+        ]: "<iframe frameborder='0' width='100%' height='315' src='//www.dailymotion.com/embed/video/"  # noqa
         + row.url
         + "' allowfullscreen></iframe>",
-        current_app.config['ATTR_VIMEO']: "<iframe src='https://player.vimeo.com/video/"  # noqa
+        current_app.config[
+            "ATTR_VIMEO"
+        ]: "<iframe src='https://player.vimeo.com/video/"  # noqa
         + row.url
         + "?color=ffffff&title=0&byline=0&portrait=0' width='640' height='360'\
-        frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>", # noqa
+        frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>",  # noqa
     }
 
 
@@ -104,9 +110,7 @@ def getVideo_and_audio(connection, cd_ref, id5, id6, id7, id8, id9):
         ORDER BY date_media DESC
     """
     req = connection.execute(
-        text(sql),
-        thiscdref=cd_ref,
-        id5=id5, id6=id6, id7=id7, id8=id8, id9=id9
+        text(sql), thiscdref=cd_ref, id5=id5, id6=id6, id7=id7, id8=id8, id9=id9
     )
     tabMedias = {"audio": list(), "video": list()}
     for r in req:
@@ -119,10 +123,10 @@ def getVideo_and_audio(connection, cd_ref, id5, id6, id7, id8, id9):
                 "author": deleteNone(r.auteur),
                 "description": deleteNone(r.desc_media),
                 "id_media": r.id_media,
-                "source":r.source,
-                "licence":r.licence
+                "source": r.source,
+                "licence": r.licence,
             }
-            if r.id_type == current_app.config['ATTR_AUDIO']:
+            if r.id_type == current_app.config["ATTR_AUDIO"]:
                 tabMedias["audio"].append(temp)
             else:
                 tabMedias["video"].append(temp)
@@ -166,14 +170,12 @@ def getPhotosGallery(connection, id1, id2):
 
 def getPhotosGalleryByGroup(connection, id1, id2, INPNgroup):
     sql = """
-        SELECT m.*, t.nom_vern, t.lb_nom
+        SELECT m.*, t.nom_vern, t.lb_nom, t.nb_obs
         FROM atlas.vm_medias m
         JOIN atlas.vm_taxons t ON t.cd_ref = m.cd_ref
         WHERE m.id_type IN  (:thisID1, :thisID2) AND t.group2_inpn = :thisGroup
         ORDER BY RANDOM()"""
-    req = connection.execute(
-        text(sql), thisID1=id1, thisID2=id2, thisGroup=INPNgroup
-    )
+    req = connection.execute(text(sql), thisID1=id1, thisID2=id2, thisGroup=INPNgroup)
     tab_photos = []
     for r in req:
         photo = _format_media(r)
@@ -183,6 +185,7 @@ def getPhotosGalleryByGroup(connection, id1, id2, INPNgroup):
         else:
             taxon_name = r.lb_nom
         photo["name"] = taxon_name
+        photo["nb_obs"] = r.nb_obs
         tab_photos.append(photo)
     return tab_photos
 
