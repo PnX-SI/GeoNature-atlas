fbc6203466df6d90648a7d262037af85ab92d731
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2022-03-23 12:23:59 +0100

    Fix db: add indexes to vm_observations_mailles

--- data/atlas/13.atlas.vm_observations_mailles.sql
+++ data/atlas/13.atlas.vm_observations_mailles.sql
@@ -1,13 +1,17 @@
 CREATE MATERIALIZED VIEW atlas.vm_observations_mailles
-AS SELECT obs.cd_ref,
-obs.id_observation,
-m.id_maille,
-m.geojson_maille,
-date_part('year', dateobs) as annee
-FROM atlas.vm_observations obs
-JOIN atlas.t_mailles_territoire m ON st_intersects(obs.the_geom_point, m.the_geom)
+AS
+    SELECT obs.cd_ref,
+        obs.id_observation,
+        m.id_maille,
+        m.geojson_maille,
+        date_part('year', dateobs) as annee
+    FROM atlas.vm_observations obs
+    JOIN atlas.t_mailles_territoire m ON st_intersects(obs.the_geom_point, m.the_geom)
 WITH DATA;
 
-create unique index on atlas.vm_observations_mailles (id_observation);
-create index on atlas.vm_observations_mailles (id_maille);
-create index on atlas.vm_observations_mailles (cd_ref);
+CREATE UNIQUE INDEX ON atlas.vm_observations_mailles (id_observation);
+CREATE INDEX ON atlas.vm_observations_mailles (id_maille);
+CREATE INDEX ON atlas.vm_observations_mailles (cd_ref);
+CREATE INDEX ON atlas.vm_observations_mailles (geojson_maille);
+CREATE INDEX ON atlas.vm_observations_mailles (annee);
+CREATE UNIQUE INDEX ON atlas.vm_observations_mailles USING btree(id_observation, geojson_maille) ;
