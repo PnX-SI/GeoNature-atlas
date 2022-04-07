a366fd95ec8717e9e861a94a930f361e1edac2c9
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2022-04-07 15:51:21 +0200

    Fix data/vm_communes: add VM for subdivided areas
    Fix #372.

--- data/atlas/7.atlas.vm_communes.sql
+++ data/atlas/7.atlas.vm_communes.sql
@@ -1,12 +1,20 @@
 -- Communes contenues entièrement dans le territoire
 
 CREATE MATERIALIZED VIEW atlas.vm_communes AS
-SELECT c.insee,
-c.commune_maj,
-c.the_geom,
-st_asgeojson(st_transform(c.the_geom, 4326)) as commune_geojson
-FROM atlas.l_communes c
-JOIN atlas.t_layer_territoire t ON ST_CONTAINS(ST_BUFFER(t.the_geom,200), c.the_geom);
+    SELECT
+        c.insee,
+        c.commune_maj,
+        c.the_geom,
+        st_asgeojson(st_transform(c.the_geom, 4326)) AS commune_geojson
+    FROM atlas.l_communes AS c
+    WHERE EXISTS (
+            SELECT 'X'
+            FROM atlas.vm_subdivided_area AS sa
+            WHERE sa.code = 'territory_buffer-200'
+                AND st_intersects(sa.geom, c.the_geom)
+        )
+    WITH DATA;
 
-CREATE UNIQUE INDEX ON atlas.vm_communes (insee);
-CREATE INDEX index_gist_vm_communes_the_geom ON atlas.vm_communes USING gist (the_geom);
+CREATE UNIQUE INDEX ON atlas.vm_communes (insee) ;
+CREATE INDEX index_gist_vm_communes_the_geom ON atlas.vm_communes USING gist (the_geom) ;
+
