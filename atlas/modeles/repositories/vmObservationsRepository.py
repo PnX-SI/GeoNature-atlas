1e70923ee1cdd6ff5c9c8e2a1ea0e39e4961057a
Andria Capai <andria_capai@natural-solutions.eu>
2022-12-14 18:26:31 +0100

    perf: improve performance for "getLastDiscoveries"
    Before, queries's duration to get 6 last discoveries : 7sec
    New query duration: 0.34 sec
    (analyze summary from DBEAVER based on atlas06 and atlas13)
    atlas13 : https://hop-levivant.fr/ (13)
    atlas06 : https://www.biodiv06-lpo.org/
    
    Reviewed-by: andriac (Natural Solutions)

--- atlas/modeles/repositories/vmObservationsRepository.py
+++ atlas/modeles/repositories/vmObservationsRepository.py
@@ -300,13 +300,16 @@ def genericStatMedias(connection, tab):
 
 def getLastDiscoveries(connection):
     sql="""
-    SELECT date(min(dateobs)), vo.cd_ref, vt.lb_nom, vt.nom_vern, m.id_media, m.chemin, m.url, vt.group2_inpn
-    FROM atlas.vm_observations vo 
-    JOIN atlas.vm_taxref vt ON vo.cd_ref = vt.cd_nom 
-    LEFT JOIN atlas.vm_medias m ON m.cd_ref=vo.cd_ref and m.id_type = :thisidtype
+    with t as (SELECT date(min(vo.dateobs)) date, vo.cd_ref
+    FROM atlas.vm_observations vo
+    GROUP BY vo.cd_ref
+    ORDER BY date desc)
+    select t.*, vt.lb_nom, vt.nom_vern, m.id_media, m.chemin, m.url, vt.group2_inpn
+    from t 
+    JOIN atlas.vm_taxref vt ON t.cd_ref = vt.cd_nom 
+    LEFT JOIN atlas.vm_medias m ON m.cd_ref=t.cd_ref and m.id_type = :thisidtype
     WHERE id_rang='ES'
-    GROUP BY vo.cd_ref, vt.lb_nom, vt.nom_vern, m.id_media, m.chemin, m.url, vt.group2_inpn
-    ORDER BY min(dateobs) DESC
+    order by t.date desc
     LIMIT 6
     """
     req = connection.execute(text(sql), thisidtype=current_app.config["ATTR_MAIN_PHOTO"])
