b77c281fd39ddf31aaeed2f210ea5651ffae5c3f
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2022-12-16 11:53:55 +0100

    feat(data): add syntheseff with AURA SINP blurring

--- data/gn2/atlas_synthese.sql
+++ data/gn2/atlas_synthese.sql
@@ -1,4 +1,7 @@
--- Creation d'une vue permettant de reproduire le contenu de la table du même nom dans les versions précédentes
+-- Compatibility : Atlas v1.5.0+
+-- Replace standard SINP blurring with AURA SINP meshes blurring
+-- Usage : replace ~/www/atlas/data/gn2/atlas_synthese.sql by this content
+
 
 CREATE MATERIALIZED VIEW synthese.vm_cor_synthese_area
 TABLESPACE pg_default
@@ -14,7 +17,7 @@ AS
 		 	ON (sa.id_area = a.id_area)
 		 JOIN ref_geo.bib_areas_types AS t
 		 	ON (a.id_type = t.id_type)
-	WHERE t.type_code IN ('M10', 'COM', 'DEP')
+	WHERE t.type_code IN ('M5', 'COM')
 WITH DATA;
 
 -- View indexes:
@@ -25,7 +28,7 @@ CREATE INDEX ON synthese.vm_cor_synthese_area (type_code);
 VACUUM ANALYSE synthese.vm_cor_synthese_area;
 
 
-CREATE OR REPLACE FUNCTION atlas.get_blurring_centroid_geom_by_code(code CHARACTER VARYING, idSynthese INTEGER)
+CREATE OR REPLACE FUNCTION atlas.get_blurring_centroid_geom_by_diffusion_level(code CHARACTER VARYING, idSynthese INTEGER)
  RETURNS geometry
  LANGUAGE plpgsql
  IMMUTABLE
@@ -37,7 +40,27 @@ AS $function$
 		SELECT INTO centroid csa.centroid_4326
 		FROM synthese.vm_cor_synthese_area AS csa
 		WHERE csa.id_synthese = idSynthese
-			AND csa.type_code = (CASE WHEN code = '1' THEN 'COM' WHEN code = '2' THEN 'M10' WHEN code = '3' THEN 'DEP' END)
+			AND csa.type_code = (CASE WHEN code = '0' THEN 'M5' WHEN code = '1' THEN 'M5' WHEN code = '2' THEN 'M5' WHEN code = '3' THEN 'M5' END)
+		LIMIT 1 ;
+
+		RETURN centroid ;
+	END;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION atlas.get_blurring_centroid_geom_by_sensitivity(code CHARACTER VARYING, idSynthese INTEGER)
+ RETURNS geometry
+ LANGUAGE plpgsql
+ IMMUTABLE
+AS $function$
+	-- Function which return the centroid for a sensitivity or diffusion_level code and a synthese id
+	DECLARE centroid geometry;
+
+	BEGIN
+		SELECT INTO centroid csa.centroid_4326
+		FROM synthese.vm_cor_synthese_area AS csa
+		WHERE csa.id_synthese = idSynthese
+			AND csa.type_code = (CASE WHEN code = '1' THEN 'M5' WHEN code = '2' THEN 'M5' WHEN code = '3' THEN 'M5' END)
 		LIMIT 1 ;
 
 		RETURN centroid ;
@@ -54,20 +77,20 @@ SELECT
 	s.observers AS observateurs,
 	(s.altitude_min + s.altitude_max) / 2 AS altitude_retenue,
 	CASE
-		WHEN (sens.cd_nomenclature::INT >= 1 AND sens.cd_nomenclature::INT <= 3 AND dl.cd_nomenclature::INT >= 1 AND dl.cd_nomenclature::INT <= 3) THEN
+		WHEN (sens.cd_nomenclature::INT >= 1 AND sens.cd_nomenclature::INT <= 3 AND dl.cd_nomenclature::INT >= 0 AND dl.cd_nomenclature::INT <= 3) THEN
 			CASE
 				WHEN (sens.cd_nomenclature::INT >= dl.cd_nomenclature::INT) THEN (
-					atlas.get_blurring_centroid_geom_by_code(sens.cd_nomenclature, s.id_synthese)
+					atlas.get_blurring_centroid_geom_by_sensitivity(sens.cd_nomenclature, s.id_synthese)
 				)
 				WHEN (sens.cd_nomenclature::INT < dl.cd_nomenclature::INT) THEN (
-					atlas.get_blurring_centroid_geom_by_code(dl.cd_nomenclature, s.id_synthese)
+					atlas.get_blurring_centroid_geom_by_diffusion_level(dl.cd_nomenclature, s.id_synthese)
 				)
 			END
-		WHEN (sens.cd_nomenclature::INT >= 1 AND sens.cd_nomenclature::INT <= 3) AND (dl.cd_nomenclature::INT < 1 OR dl.cd_nomenclature::INT > 3) THEN (
-			atlas.get_blurring_centroid_geom_by_code(sens.cd_nomenclature, s.id_synthese)
+		WHEN (sens.cd_nomenclature::INT >= 1 AND sens.cd_nomenclature::INT <= 3) AND (dl.cd_nomenclature::INT > 4) THEN (
+			atlas.get_blurring_centroid_geom_by_sensitivity(sens.cd_nomenclature, s.id_synthese)
 		)
-		WHEN (dl.cd_nomenclature::INT >= 1 AND dl.cd_nomenclature::INT <= 3) AND (sens.cd_nomenclature::INT < 1 OR sens.cd_nomenclature::INT > 3) THEN (
-			atlas.get_blurring_centroid_geom_by_code(dl.cd_nomenclature, s.id_synthese)
+		WHEN (dl.cd_nomenclature::INT >= 0 AND dl.cd_nomenclature::INT <= 3) AND (sens.cd_nomenclature::INT < 1) THEN (
+			atlas.get_blurring_centroid_geom_by_diffusion_level(dl.cd_nomenclature, s.id_synthese)
 		)
 		ELSE st_transform(s.the_geom_point, 4326)
 	END AS the_geom_point,
