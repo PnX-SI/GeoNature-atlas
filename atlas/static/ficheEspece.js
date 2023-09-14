b9414e50effa389b9ba8465c29f87d2880913186
Amandine <amandine.sahl@cevennes-parcnational.fr>
2023-09-14 10:30:55 +0200

    fix(photo-gallery): add link to species sheet on Lightbox photo (#487)
    Co-authored-by: Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>

--- atlas/static/ficheEspece.js
+++ atlas/static/ficheEspece.js
@@ -1,7 +1,6 @@
 // Change tooltip and glyphycon for media block
-
-  var currentDate = new Date();
-  YEARMAX = currentDate.getFullYear();
+var currentDate = new Date();
+YEARMAX = currentDate.getFullYear();
 
 var myBoolean = true;
 $('#btn-more-audio').click(function(){
@@ -31,15 +30,15 @@ $('.accordion-toggle').click(function(){
 	$(this).find('span').toggleClass('fas fa-chevron-down').toggleClass('fas fa-chevron-up');
 })
 
-
 $("[rel=tooltip]").tooltip();
 
-
-
 // Other information accordion
-
-
 $('.collapse').on('show.bs.collapse', function () {
     $('.collapse.in').collapse('hide');
 });
 
+jQuery(function () {
+    lightbox.option({
+        "albumLabel": "Image %1 sur %2"
+    })
+});
