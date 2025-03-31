be4d904cbe7bab66b8944394e50a345f9a7f75ff
Joaquim Nallar <jna@makina-corpus.com>
2025-03-31 17:40:54 +0200

    fix taxon filter list

--- atlas/static/listeEspeces.js
+++ atlas/static/listeEspeces.js
@@ -1,24 +1,31 @@
 $(".lazy").lazy({
-          effect: "fadeIn",
-          effectTime: 2000,
-          threshold: 0,
-          appendScroll: $("#taxonList")
-        });
+    effect: "fadeIn",
+    effectTime: 2000,
+    threshold: 0,
+    appendScroll: $("#taxonList")
+});
 $('[data-toggle="tooltip"]').tooltip();
-$(document).ready(function(){
-  function filterListeEspèces() {
-    let name = document.querySelector("#taxonInput").value.toLowerCase();
-    let onlyProtégées = document.querySelector("#filtreProtégées").checked;
-    let onlyPatrimoniales = document.querySelector("#filtrePatrimoniales").checked;
-    $("#taxonList li").each(function() {
-      let matched = $(this).text().toLowerCase().indexOf(name) > -1
-        && (!onlyProtégées || "data-protégée" in this.attributes)
-        && (!onlyPatrimoniales || "data-patrimoniale" in this.attributes);
-      $(this).toggle(matched);
-    });
-  }
 
-  document.querySelectorAll("#filtresListeEspèces input").forEach(
-    (element) => element.addEventListener("input", filterListeEspèces)
-  )
+var taxonDomElem = Array.from(document.querySelectorAll("#taxonList li"))
+
+$(document).ready(function(){
+    function filterListeEspèces() {
+        let name = document.querySelector("#taxonInput").value.toLowerCase();
+        let onlyProtégées = document.querySelector("#filtreProtégées").checked;
+        let onlyPatrimoniales = document.querySelector("#filtrePatrimoniales").checked;
+
+        const filteredTaxonDomElem = taxonDomElem.filter(elem => {
+            let matched = elem.innerText.toLowerCase().indexOf(name) > -1
+                && (!onlyProtégées || "data-protégée" in elem.attributes)
+                && (!onlyPatrimoniales || "data-patrimoniale" in elem.attributes);
+            if (matched) {
+                return true;
+            }
+        })
+        document.querySelectorAll("#taxonList li").forEach(e => e.remove());
+        document.querySelector("#taxonList ul").append(...filteredTaxonDomElem)
+    }
+    document.querySelectorAll("#filtresListeEspèces input").forEach(
+        (element) => element.addEventListener("input", filterListeEspèces)
+    )
 });
