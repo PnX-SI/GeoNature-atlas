b83a88cfef96446fe495a80d1de81834b263d270
Orangetine <salima.rakass@gmail.com>
2025-05-27 09:58:52 +0200

    création de la fonction commune get_session

--- atlas/modeles/utils.py
+++ atlas/modeles/utils.py
@@ -1,7 +1,15 @@
 # -*- coding:utf-8 -*-
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+from atlas.configuration.config import SQLALCHEMY_DATABASE_URI
 
+from contextlib import contextmanager
 import unicodedata
 
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 def deleteAccent(string):
     if string is None:
@@ -16,3 +24,12 @@ def findPath(row):
         return row.chemin
     else:
         return row.url
+
+@contextmanager   
+def get_session():
+    """Créer une session pour les opérations sur la base."""
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
