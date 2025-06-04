83bec47392ca8152317f9d7bd4a36c820f5d14a5
Orangetine <salima.rakass@gmail.com>
2025-06-04 11:23:55 +0200

    Passage en ORM de la vmStatsStatutTaxonComm

--- atlas/modeles/repositories/vmStatsStatutTaxonCommRepository.py
+++ atlas/modeles/repositories/vmStatsStatutTaxonCommRepository.py
@@ -1,21 +1,31 @@
 # -*- coding:utf-8 -*-
 
-from sqlalchemy.sql import text
+from sqlalchemy.sql import select, func
+from atlas.modeles.entities.vmObservations import VmObservations
+from atlas.modeles.entities.vmTaxons import VmTaxons
+from atlas.modeles.entities.vmAreas import VmAreas
 
 
 # get nombre taxons protégés et nombre taxons patrimoniaux par communes
-def get_nb_taxon_pro_pat_area(connection, id_area):
-    sql = """
-SELECT
-    COUNT(t.patrimonial) AS nb_taxon_patrimonial, COUNT(t.protection_stricte) AS nb_taxon_protege
-FROM atlas.vm_observations o
-         JOIN atlas.vm_taxons t ON t.cd_ref=o.cd_ref
-         JOIN atlas.vm_l_areas area ON st_intersects(o.the_geom_point, area.the_geom)
-WHERE area.id_area = :thisIdArea
-    """
-    req = connection.execute(text(sql), {"thisIdArea":id_area})
+def get_nb_taxon_pro_pat_area(session, id_area):
+    req = (
+        select(
+            func.count(VmTaxons.patrimonial)\
+                .label("nb_taxon_patrimonial"),
+            func.count(VmTaxons.protection_stricte)\
+                .label("nb_taxon_protege")
+        )
+        .select_from(VmObservations)
+        .join(VmTaxons, VmTaxons.cd_ref == VmObservations.cd_ref)
+        .join(
+            VmAreas, 
+            func.ST_intersects(VmObservations.the_geom_point, VmAreas.the_geom)
+        )
+        .filter(VmAreas.id_area == id_area)
+    )   
+    results = session.execute(req).all() 
     taxonProPatri = dict()
-    for r in req:
+    for r in results:
         taxonProPatri = {"nbTaxonPro": r.nb_taxon_protege, 
                          "nbTaxonPatri": r.nb_taxon_patrimonial}
     return taxonProPatri
