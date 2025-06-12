dd3615317bc8e39fd0f9c97dd29c28c5cfb6f07d
Orangetine <salima.rakass@gmail.com>
2025-06-12 14:59:34 +0200

    correction requête listeTaxonsSearch

--- atlas/modeles/repositories/vmSearchTaxonRepository.py
+++ atlas/modeles/repositories/vmSearchTaxonRepository.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from sqlalchemy import desc, func
+from sqlalchemy import desc, func, select
 
 from atlas.modeles.entities.vmSearchTaxon import VmSearchTaxon
 
@@ -20,19 +20,23 @@ def listeTaxonsSearch(session, search, limit=50):
         value = cd_ref
     """
 
-    req = session.query(
-        VmSearchTaxon.search_name,
-        VmSearchTaxon.cd_ref,
-        func.similarity(VmSearchTaxon.search_name, search).label("idx_trgm"),
-    ).distinct()
-
+    idx_trgm_col = func.similarity(VmSearchTaxon.search_name, search).label("idx_trgm")
     search = search.replace(" ", "%")
-    req = (
-        req.filter(VmSearchTaxon.search_name.ilike("%" + search + "%"))
-        .order_by(desc("idx_trgm"))
-        .order_by(VmSearchTaxon.cd_ref == VmSearchTaxon.cd_nom)
+    subreq = (
+        select(
+            VmSearchTaxon.cd_ref,
+            VmSearchTaxon.search_name,
+            idx_trgm_col
+        )
+        .filter(VmSearchTaxon.search_name.ilike("%" + search + "%"))
+        .order_by(
+            desc(idx_trgm_col),                         
+            VmSearchTaxon.cd_ref == VmSearchTaxon.cd_nom  
+        )
         .limit(limit)
+        .subquery()
     )
+    req = session.query(subreq.c.search_name, subreq.c.cd_ref, subreq.c.idx_trgm).distinct()
     data = req.all()
 
     return [{"label": d[0], "value": d[1]} for d in data]
