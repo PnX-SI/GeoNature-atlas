415cf8f100d048c15eceb9cec0a9935e5d38e179
Orangetine <salima.rakass@gmail.com>
2025-06-13 12:00:04 +0200

    Passage syntaxe SQLAlchemy2 VmTaxons, vmTaxref

--- atlas/modeles/entities/vmTaxons.py
+++ atlas/modeles/entities/vmTaxons.py
@@ -1,81 +1,82 @@
 # -*- coding:utf-8 -*-
 
-from sqlalchemy import Column, Integer, MetaData, String, Table, Float, Text, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Float, Text, ForeignKey
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import relationship
 
-from atlas.env import db
+from typing import List
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 
 class VmTaxons(Base):
     __tablename__ = "vm_taxons"
     __table_args__ = {"schema" : "atlas"}
 
-    cd_ref = Column(Integer, primary_key=True)
-    regne = Column(String(20))
-    phylum = Column(String(50))
-    classe = Column(String(50)) 
-    ordre = Column(String(50))
-    famille = Column(String(50))
-    cd_taxsup = Column(Integer)
-    lb_nom = Column(String(100))
-    lb_auteur = Column(String(250))
-    nom_complet = Column(String(255))
-    nom_valide = Column(String(255))
-    nom_vern = Column(String(1000))
-    nom_vern_eng = Column(String(500))
-    group1_inpn = Column(String(50))
-    group2_inpn = Column(String(50))
-    nom_complet_html = Column(String(500))
-    id_rang = Column(String(10)) 
-    patrimonial = Column(String(255))
-    protection_stricte = Column(String(255))
-    yearmin = Column(Float(53))
-    yearmax = Column(Float(53))
-    nb_obs = Column(Integer)
-    attributs = relationship("VmCorTaxonAttribut", back_populates="taxon")
-    organisms = relationship("VmCorTaxonOrganism", back_populates="taxon")
+    cd_ref: Mapped[int] = mapped_column(primary_key=True)
+    regne: Mapped[str] = mapped_column(String(20))
+    phylum: Mapped[str] = mapped_column(String(50))
+    classe: Mapped[str] = mapped_column(String(50)) 
+    ordre: Mapped[str] = mapped_column(String(50))
+    famille: Mapped[str] = mapped_column(String(50))
+    cd_taxsup: Mapped[int] = mapped_column()
+    lb_nom: Mapped[str] = mapped_column(String(100))
+    lb_auteur: Mapped[str] = mapped_column(String(250))
+    nom_complet: Mapped[str] = mapped_column(String(255))
+    nom_valide: Mapped[str] = mapped_column(String(255))
+    nom_vern: Mapped[str] = mapped_column(String(1000))
+    nom_vern_eng: Mapped[str] = mapped_column(String(500))
+    group1_inpn: Mapped[str] = mapped_column(String(50))
+    group2_inpn: Mapped[str] = mapped_column(String(50))
+    nom_complet_html: Mapped[str] = mapped_column(String(500))
+    id_rang: Mapped[str] = mapped_column(String(10)) 
+    patrimonial: Mapped[str] = mapped_column(String(255))
+    protection_stricte: Mapped[str] = mapped_column(String(255))
+    yearmin: Mapped[float] = mapped_column(Float(53))
+    yearmax: Mapped[float] = mapped_column(Float(53))
+    nb_obs: Mapped[int] = mapped_column()
+    attributs: Mapped[List["VmCorTaxonAttribut"]] = relationship("VmCorTaxonAttribut", back_populates="taxon")
+    organisms: Mapped[List["VmCorTaxonOrganism"]] = relationship("VmCorTaxonOrganism", back_populates="taxon")
 
 class VmCorTaxonAttribut(Base):
     __tablename__ = "vm_cor_taxon_attribut"
     __table_args__ = {"schema" : "atlas"}
 
-    id_attribut = Column(Integer, primary_key=True)
-    cd_ref = Column(Integer, ForeignKey("atlas.vm_taxons.cd_ref"), primary_key=True)
-    valeur_attribut = Column(Text)
-    taxon = relationship("VmTaxons", back_populates="attributs")
+    id_attribut: Mapped[int] = mapped_column(primary_key=True)
+    cd_ref: Mapped[int] = mapped_column(ForeignKey("atlas.vm_taxons.cd_ref"), primary_key=True)
+    valeur_attribut: Mapped[str] = mapped_column(Text)
+    taxon: Mapped["VmTaxons"] = relationship("VmTaxons", back_populates="attributs")
 
 class VmCorTaxonOrganism(Base):
     __tablename__ = "vm_cor_taxon_organism"
     __table_args__ = {"schema" : "atlas"}
 
-    cd_ref = Column(Integer, ForeignKey("atlas.vm_taxons.cd_ref"), primary_key=True)
-    id_organism = Column(Integer, primary_key=True)
+    cd_ref: Mapped[int] = mapped_column(ForeignKey("atlas.vm_taxons.cd_ref"), primary_key=True)
+    id_organism: Mapped[int] = mapped_column(primary_key=True)
 
-    nb_observations = Column(Integer)
-    nom_organism = Column(String(500))
-    adress_organism = Column(String(128))
-    cp_organism = Column(String(5))
-    ville_organism = Column(String(100))
-    tel_organism = Column(String(14))
-    email_organism = Column(String(100))
-    url_organism = Column(String(255))
-    url_logo = Column(String(255))
-    taxon = relationship("VmTaxons", back_populates="organisms")
+    nb_observations: Mapped[int] = mapped_column()
+    nom_organism: Mapped[str] = mapped_column(String(500))
+    adress_organism: Mapped[str] = mapped_column(String(128))
+    cp_organism: Mapped[str] = mapped_column(String(5))
+    ville_organism: Mapped[str] = mapped_column(String(100))
+    tel_organism: Mapped[str] = mapped_column(String(14))
+    email_organism: Mapped[str] = mapped_column(String(100))
+    url_organism: Mapped[str] = mapped_column(String(255))
+    url_logo: Mapped[str] = mapped_column(String(255))
+    taxon: Mapped["VmTaxons"] = relationship("VmTaxons", back_populates="organisms")
 
 class VmTaxonsMostView(Base):
     __tablename__ = "vm_taxons_plus_observes"
     __table_args__ = {"schema" : "atlas"}
 
-    cd_ref = Column(Integer, primary_key = True)
-    nb_obs = Column(Integer)
-    lb_nom = Column(String(250))
-    group2_inpn = Column(String(50))
-    nom_vern = Column(String(1000))
-    id_media = Column(Integer)
-    url = Column(String(255))
-    chemin = Column(String(255))
-    id_type = Column(Integer)
-
+    cd_ref: Mapped[int] = mapped_column(primary_key = True)
+    nb_obs: Mapped[int] = mapped_column()
+    lb_nom: Mapped[str] = mapped_column(String(250))
+    group2_inpn: Mapped[str] = mapped_column(String(50))
+    nom_vern: Mapped[str] = mapped_column(String(1000))
+    id_media: Mapped[int] = mapped_column()
+    url: Mapped[str] = mapped_column(String(255))
+    chemin: Mapped[str] = mapped_column(String(255))
+    id_type: Mapped[int] = mapped_column()
--- atlas/modeles/entities/vmTaxref.py
+++ atlas/modeles/entities/vmTaxref.py
@@ -1,40 +1,38 @@
 # coding: utf-8
-from sqlalchemy import Column, Integer, MetaData, String, Text, Table
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Text
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
 
-from atlas.env import db
-
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 
 class VmTaxref(Base):
     __tablename__ = "vm_taxref"
     __table_args__ = {"schema" : "atlas"}
 
-    cd_nom = Column(Integer, primary_key = True)
-    id_statut = Column(String(1))
-    id_habitat = Column(Integer)
-    id_rang = Column(String(4))
-    regne = Column(String(20))
-    phylum = Column(String(50))
-    classe = Column(String(50))
-    ordre = Column(String(50))
-    famille = Column(String(50))
-    sous_famille = Column(String(50))
-    tribu = Column(String(50))
-    cd_taxsup = Column(Integer)
-    cd_sup = Column(Integer)
-    cd_ref = Column(Integer, index=True)
-    lb_nom = Column(String(100), index=True)
-    lb_auteur = Column(String(250))
-    nom_complet = Column(String(255), index=True)
-    nom_complet_html = Column(String(500))
-    nom_valide = Column(String(255) , index=True)
-    nom_vern = Column(String(1000))
-    nom_vern_eng = Column(String(500))
-    group1_inpn = Column(String(50))
-    group2_inpn = Column(String(50))
-    url = Column(Text)
-    group3_inpn = Column(String(250))
-
-    
+    cd_nom: Mapped[int] = mapped_column(primary_key = True)
+    id_statut: Mapped[str] = mapped_column(String(1))
+    id_habitat: Mapped[int] = mapped_column()
+    id_rang: Mapped[str] = mapped_column(String(4))
+    regne: Mapped[str] = mapped_column(String(20))
+    phylum: Mapped[str] = mapped_column(String(50))
+    classe: Mapped[str] = mapped_column(String(50))
+    ordre: Mapped[str] = mapped_column(String(50))
+    famille: Mapped[str] = mapped_column(String(50))
+    sous_famille: Mapped[str] = mapped_column(String(50))
+    tribu: Mapped[str] = mapped_column(String(50))
+    cd_taxsup: Mapped[int] = mapped_column()
+    cd_sup: Mapped[int] = mapped_column()
+    cd_ref: Mapped[int] = mapped_column(index=True)
+    lb_nom: Mapped[str] = mapped_column(String(100), index=True)
+    lb_auteur: Mapped[str] = mapped_column(String(250))
+    nom_complet: Mapped[str] = mapped_column(String(255), index=True)
+    nom_complet_html: Mapped[str] = mapped_column(String(500))
+    nom_valide: Mapped[str] = mapped_column(String(255) , index=True)
+    nom_vern: Mapped[str] = mapped_column(String(1000))
+    nom_vern_eng: Mapped[str] = mapped_column(String(500))
+    group1_inpn: Mapped[str] = mapped_column(String(50))
+    group2_inpn: Mapped[str] = mapped_column(String(50))
+    url: Mapped[str] = mapped_column(Text)
+    group3_inpn: Mapped[str] = mapped_column(String(250))
