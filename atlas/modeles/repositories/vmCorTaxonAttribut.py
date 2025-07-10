13e30ef7cea7846f033f523f511af47744f35382
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-07-10 17:55:35 +0200

    feat(speciesSheet): use Markdown for custom Taxhub attributes in infos bloc

--- atlas/modeles/repositories/vmCorTaxonAttribut.py
+++ atlas/modeles/repositories/vmCorTaxonAttribut.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from sqlalchemy.sql import text
+import markdown
 
 
 def getAttributesTaxon(connection, cd_ref, displayed_attr):
@@ -14,5 +15,7 @@ def getAttributesTaxon(connection, cd_ref, displayed_attr):
 
     desc_taxon = []
     for row in results:
-        desc_taxon.append({"code": row.code, "title": row.title, "value": row.value})
+        desc_taxon.append(
+            {"code": row.code, "title": row.title, "value": markdown.markdown(row.value)}
+        )
     return desc_taxon
