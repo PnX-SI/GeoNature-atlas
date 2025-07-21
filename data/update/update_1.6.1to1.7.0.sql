70a9dd4079362a9dfb6764cfbec26addb7850a47
TheoLechemia <lechemia.theo@gmail.com>
2025-07-21 11:16:59 +0200

    refact to 1.7

--- data/update/update_1.6.1to1.7.0.sql
+++ data/update/update_1.6.1to1.7.0.sql
@@ -1,7 +1,24 @@
 -- Vérifier les évolutions de BDD sur https://github.com/PnX-SI/GeoNature-atlas/pull/629/files#diff-1b1113bdb3d6f07008e26543f9033ea896c3f2d6d133e1233abe19d4b8f07601
 
-DROP TABLE atlas.t_mailles_territoire;
+DROP TABLE atlas.t_mailles_territoire CASCADE;
+DROP MATERIALIZED VIEW atlas.vm_medias;
 
+
+CREATE MATERIALIZED VIEW atlas.vm_medias AS
+   SELECT t_medias.id_media,
+      t_medias.cd_ref,
+      t_medias.titre,
+      t_medias.url,
+      t_medias.chemin,
+      t_medias.auteur,
+      t_medias.desc_media,
+      t_medias.date_media,
+      t_medias.id_type,
+      t_medias.licence,
+      t_medias.source
+   FROM taxonomie.t_medias;
+
+CREATE UNIQUE INDEX ON atlas.vm_medias(id_media);
 CREATE MATERIALIZED VIEW atlas.vm_mailles_territoire AS
     SELECT
         st_transform(a.geom, 4326) AS the_geom,
@@ -20,25 +37,20 @@ CREATE UNIQUE INDEX ON atlas.vm_mailles_territoire
 CREATE INDEX ON atlas.vm_mailles_territoire
     USING spgist (the_geom);
 
--- Création index sur les mailles du territoire
-CREATE INDEX ON atlas.t_mailles_territoire
-    USING spgist (the_geom);
 
-DROP MATERIALIZED VIEW IF EXISTS atlas.vm_observations_mailles;
-
-CREATE MATERIALIZED VIEW atlas.vm_observations_mailles AS
-    SELECT
-        o.cd_ref,
-        date_part('year', o.dateobs) AS annee,
-        m.id_maille,
-        COUNT(o.id_observation) AS nbr,
-        ARRAY_AGG(o.id_observation) AS id_observations
-    FROM atlas.vm_observations AS o
-        JOIN atlas.t_mailles_territoire AS m
-            ON (o.the_geom_point && m.the_geom)
-    GROUP BY o.cd_ref, date_part('year', o.dateobs), m.id_maille
-    ORDER BY o.cd_ref, annee
-WITH DATA;
+ CREATE MATERIALIZED VIEW atlas.vm_observations_mailles AS
+        SELECT
+            o.cd_ref,
+            date_part('year', o.dateobs) AS annee,
+            m.id_maille,
+            COUNT(o.id_observation) AS nbr,
+            ARRAY_AGG(o.id_observation) AS id_observations
+        FROM atlas.vm_observations AS o
+            JOIN atlas.vm_mailles_territoire AS m
+                ON (o.the_geom_point && m.the_geom)
+        GROUP BY o.cd_ref, date_part('year', o.dateobs), m.id_maille
+        ORDER BY o.cd_ref, annee
+    WITH DATA;
 
 CREATE UNIQUE INDEX ON atlas.vm_observations_mailles
     USING btree (cd_ref, annee, id_maille);
@@ -56,25 +68,13 @@ CREATE INDEX ON atlas.vm_observations_mailles
 CREATE INDEX index_gist_t_layer_territoire ON atlas.t_layer_territoire USING gist(the_geom);
 CREATE INDEX index_gist_t_layers_communes ON atlas.l_communes USING gist (the_geom);
 
-DROP MATERIALIZED VIEW IF EXISTS atlas.vm_medias;
-CREATE MATERIALIZED VIEW atlas.vm_medias AS
-   SELECT t_medias.id_media,
-      t_medias.cd_ref,
-      t_medias.titre,
-      t_medias.url,
-      t_medias.chemin,
-      t_medias.auteur,
-      t_medias.desc_media,
-      t_medias.date_media,
-      t_medias.id_type,
-      t_medias.licence,
-      t_medias.source
-   FROM taxonomie.t_medias
-WHERE NOT t_medias.supprime = true;
-   FROM taxonomie.t_medias;
+
 
 -- Faut faire des GRAND SELECT sur les 3 VM créées ?
 -- Voir https://github.com/PnX-SI/GeoNature-atlas/pull/629/files#diff-e53167aeace735e10049b339b3f045aa65f7f994bfd6f0d6143861d4110ec186R39
 -- Mais on ne connait pas le nom de l'utilisateur qui n'est pas forcément geonatatlas ?
 -- Dans les précédentes UPDATE, on le mettait quand même en dur car quasiment tout le monde utilise ce nom... https://github.com/PnX-SI/GeoNature-atlas/blob/develop/data/update/update_1.5.2to1.6.0.sql#L85
 -- Donc on peut faire ça je pense
+
+drop cascades to materialized view atlas.vm_observations_mailles
+drop cascades to materialized view atlas.vm_taxons_plus_observes
