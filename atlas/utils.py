c396869a507ad749509cac0446bca0e80247f727
TheoLechemia <lechemia.theo@gmail.com>
2025-08-08 15:50:17 +0200

    remove translations used in v2

--- atlas/utils.py
+++ atlas/utils.py
@@ -92,19 +92,3 @@ def get_locale():
     if current_app.config["MULTILINGUAL"]:
         return g.lang_code
     return current_app.config["DEFAULT_LANGUAGE"]
-
-
-def get_tranlated_labels():
-    """
-    Return a dict of translated labels regarding the context of the app (multiple areas or not)
-    """
-    isOnlyMunicipalities = False
-    if current_app.config["TYPE_TERRITOIRE_SHEET"] == ["COM"]:
-        isOnlyMunicipalities = True
-    return {
-        "territories": (
-            gettext("municipalities") if isOnlyMunicipalities else gettext("territories")
-        ),
-        "territory": gettext("municipality") if isOnlyMunicipalities else gettext("territory"),
-        "search_area": gettext("search.city") if isOnlyMunicipalities else gettext("search.area"),
-    }
