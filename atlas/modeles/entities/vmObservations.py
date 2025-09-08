3864d0b29dcb91d29ad797c6279fc3e5fa876ec5
TheoLechemia <lechemia.theo@gmail.com>
2025-09-08 12:18:06 +0200

    add missing properties in vmobsertionsmaille

--- atlas/modeles/entities/vmObservations.py
+++ atlas/modeles/entities/vmObservations.py
@@ -43,6 +43,8 @@ class VmObservationsMailles(db.Model):
     __table_args__ = {"schema": "atlas"}
 
     id_maille: Mapped[int] = mapped_column(primary_key=True, index=True)
+    cd_ref: Mapped[int] = mapped_column()
     nbr: Mapped[int] = mapped_column()
+    annee: Mapped[int] = mapped_column()
     id_observations: Mapped[List[int]] = mapped_column(ARRAY(Integer))
     type_code: Mapped[str] = mapped_column(String(25))
