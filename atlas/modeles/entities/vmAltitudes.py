14a8aedd3551744b94f4357d1a74cdf165e2f418
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2025-10-05 19:39:23 +0200

    fix: build altitudes entity dynamicly

--- atlas/modeles/entities/vmAltitudes.py
+++ atlas/modeles/entities/vmAltitudes.py
@@ -1,18 +1,32 @@
-# coding: utf-8
+from itertools import pairwise
+from flask import current_app
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer
 from atlas.env import db
 
 
-class VmAltitudes(db.Model):
-    __tablename__ = "vm_altitudes"
-    __table_args__ = {"schema": "atlas"}
+def create_vm_altitudes_class():
+    """
+    Dynamically creates and returns the VmAltitudes model class
+    based on the altitude ranges defined in the configuration.
+    """
+    altitude_ranges = current_app.config["ALTITUDE_RANGES"]
+    print("LAAA", altitude_ranges)
 
-    cd_ref: Mapped[int] = mapped_column(primary_key=True)
-    _0_500: Mapped[int] = mapped_column()
-    _500_1000: Mapped[int] = mapped_column()
-    _1000_1500: Mapped[int] = mapped_column()
-    _1500_2000: Mapped[int] = mapped_column()
-    _2000_2500: Mapped[int] = mapped_column()
-    _2500_3000: Mapped[int] = mapped_column()
-    _3000_3500: Mapped[int] = mapped_column()
-    _3500_4000: Mapped[int] = mapped_column()
+    class_attributes = {
+        "__tablename__": "vm_altitudes",
+        "__table_args__": {"schema": "atlas"},
+        # Fixed column
+        "cd_ref": mapped_column(Integer, primary_key=True),
+    }
+
+    for alt_min, alt_max in pairwise(altitude_ranges):
+        # The '_' prefix is necessary because a variable name cannot start with a number.
+        column_name = f"_{alt_min}_{alt_max}"
+        class_attributes[column_name] = mapped_column(Integer)
+
+    VmAltitudesClass = type("VmAltitudes", (db.Model,), class_attributes)
+    return VmAltitudesClass
+
+
+VmAltitudes = create_vm_altitudes_class()
