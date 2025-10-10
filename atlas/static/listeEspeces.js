8be7e7cb88ff65628af48ffb39190e475d510e76
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2025-10-10 17:29:33 +0200

    fix(listEspeces): restore filter functionality

--- atlas/static/listeEspeces.js
+++ atlas/static/listeEspeces.js
@@ -1,15 +1,25 @@
-$(".lazy").lazy({
-          effect: "fadeIn",
-          effectTime: 2000,
-          threshold: 0,
-          appendScroll: $("#taxonList")
-        });
-$('[data-toggle="tooltip"]').tooltip();
-$(document).ready(function(){
-  $("#taxonInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#taxonList li").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+function initializePlugins() {
+    $(".lazy").lazy({
+        effect: "fadeIn",
+        effectTime: 2000,
+        threshold: 0,
+        appendScroll: $("#taxonList"),
     });
-  });
+}
+
+function filterText() {
+    $("#taxonInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+
+        $("#taxonList li").filter(function () {
+            console.log($(this).text().toLowerCase().replace(/\s+/g, " "), value);
+            var match = $(this).text().toLowerCase().replace(/\s+/g, " ").indexOf(value) > -1;
+            this.style.setProperty("display", match ? "flex" : "none", "important");
+        });
+    });
+}
+
+$(document).ready(function () {
+    initializePlugins();
+    filterText();
 });
