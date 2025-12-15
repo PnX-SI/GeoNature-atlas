b909a051b43bab58eaf043d5b5dfcfc4e8e7bc5c
lpofredc <frederic.cloitre@lpo.fr>
2025-12-15 16:13:47 +0100

    code ling

--- docs/conf.py
+++ docs/conf.py
@@ -6,25 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'GeoNature-atlas'
-copyright = '2025, GeoNature Community'
-author = 'GeoNature Community'
-release = '2.0.0-dev'
+project = "GeoNature-atlas"
+copyright = "2025, GeoNature Community"
+author = "GeoNature Community"
+release = "2.0.0-dev"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ["myst_parser"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-language = 'fr'
+language = "fr"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
 
 myst_heading_anchors = 3
