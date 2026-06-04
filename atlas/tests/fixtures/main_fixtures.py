173c394c538240957fbc2bb57795e1be1da8b428
TheoLechemia <lechemia.theo@gmail.com>
2026-06-04 14:39:35 +0200

    add some tests

--- atlas/tests/fixtures/main_fixtures.py
+++ atlas/tests/fixtures/main_fixtures.py
@@ -1,14 +1,17 @@
 from dataclasses import dataclass
 from copy import deepcopy
 
+from datetime import date
+
 import pytest
 from bs4 import BeautifulSoup
 
 from flask import template_rendered
+from geoalchemy2.elements import WKTElement
 
 from atlas.modeles.entities.vmTaxons import VmTaxons
 from atlas.modeles.entities.vmTaxref import VmTaxref
-from atlas.modeles.entities.vmAreas import VmAreasWithObs
+from atlas.modeles.entities.vmAreas import VmAreasWithObs, VmCorAreaSynthese
 from atlas.env import db
 
 
@@ -108,3 +111,48 @@ def vm_areas_with_obs_data():
     db.session.commit()
     return [area1, area2]
     # Pas de suppression ici (conformément aux instructions)
+
+
+@pytest.fixture
+def obs_with_area(db_session, taxon):
+    """Crée une observation liée au `taxon` fixture et ajoute une
+    entrée `VmCorAreaSynthese`"""
+
+    from atlas.modeles.entities.vmObservations import VmObservations
+
+    obs = VmObservations(
+        id_observation=1,
+        dateobs=date.today(),
+        observateurs="tester",
+        altitude_retenue=10,
+        cd_ref=taxon.cd_ref,
+        the_geom_point=WKTElement("POINT(1 1)", srid=4326),
+        geojson_point="{}",
+        cd_sensitivity=1,
+        id_dataset=1,
+    )
+    db_session.add(obs)
+
+    obs = VmObservations(
+        id_observation=2,
+        dateobs=date.today(),
+        observateurs="tester",
+        altitude_retenue=10,
+        cd_ref=taxon.cd_ref,
+        the_geom_point=WKTElement("POINT(2 2)", srid=4326),
+        geojson_point="{}",
+        cd_sensitivity=1,
+        id_dataset=1,
+    )
+    db_session.add(obs)
+
+    synth = VmCorAreaSynthese(
+        id_synthese=1,
+        id_area=777777,
+        id_type=1,
+        type_code="COMMUNE",
+        is_valid_for_display=True,
+    )
+    db_session.add(synth)
+    db_session.flush()
+    return obs
