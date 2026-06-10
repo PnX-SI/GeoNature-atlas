d31c1c9d85ceca106ba7c1df12b2ea161d1bcb35
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2026-06-10 11:22:15 +0200

    Add comment in vm_cor_maille_observation.sql

--- data/atlas/15.vm_cor_maille_observation.sql
+++ data/atlas/15.vm_cor_maille_observation.sql
@@ -1,3 +1,6 @@
+-- Cette VM permet de stocker la maille d'affichage (floutée ou non) d'une observation, quand on les affiche au niveau Maille.
+-- C'est une selection d'un seul zonage par observation, basée sur gn_synthese.cor_area_synthese
+
 CREATE MATERIALIZED VIEW atlas.vm_cor_maille_observation AS
     SELECT
         cas.id_area AS id_maille,
